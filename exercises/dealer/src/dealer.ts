c99c15a3e6062f8fa821a4f1a0ef6ae2a469dff0
LeeBoy <jobboy19890101@gmail.com>
2018-06-16 14:42:06 +0800

    fix: explore enum

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,46 @@ function shuffleArray(a: any[]) {
   }
 }
 
-// export class Dealer {
+export enum Suit {
+  Clubs, Diamonds, Hearts, Spades
+}
 
-// }
+export enum CardNumber {
+  Ace, Two, Three, Four, Five,
+  Six, Seven, Eight, Nine, Ten,
+  Jack, Queen, King
+}
+
+type Card = [Suit, CardNumber]
+
+let c: Card = [Suit.Clubs, CardNumber.Seven]
+
+function createDeck(): Card[] {
+  let cards: Card[] = [];
+  for (let s = 0; s < Object.keys(Suit).length; s+=2) {
+    for (let n = 0; n < Object.keys(CardNumber).length; n+=2) {
+      cards.push([s/2, n/2]);
+    }
+  }
+  return cards
+}
+
+export class Dealer {
+  cards: Card[] = []
+  constructor() {
+    this.cards = createDeck()
+  }
+ 
+  // deals 5 cards [Suit(0-3), Number(0-12)]
+  dealHand(numCards: number): Card[] {
+    return []
+  }
+  // number of cards left in the deck
+  getLength(): number {
+    return this.cards.length
+  }
+  readCard(card: Card): string {
+    return ''
+  }
+}
+
