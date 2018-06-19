0ce97cec5e1e9343402cfa34954b0aee9608eb09
LeeBoy <jobboy19890101@gmail.com>
2018-06-19 18:21:42 +0800

    fix: Challenge 4 cashier

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,34 @@
-// export function cashier() {
-//   return {};
-// }
+interface CartItem {
+  name: string;
+  price: number;
+  qty: number;
+}
+
+interface CartAPI {
+  length: number;
+  total: number;
+  addItem(item: CartItem): CartAPI;
+  add(name: string, price: number, qty: number): CartAPI;
+}
+
+export function cashier(): CartAPI {
+  let items: CartItem[] = [];
+  return {
+    get length() {
+      return items.reduce((accumulator, cartItem) => accumulator += cartItem.qty, 0)
+    },
+    get total() {
+      return items.reduce((accumulator, cartItem) => accumulator += (cartItem.qty * cartItem.price), 0)
+    },
+    addItem(item): CartAPI {
+      items.push(item);
+      return this;
+    },
+    add(name, price, qty = 1): CartAPI {
+      items.push({
+        name, price, qty
+      })
+      return this;
+    }
+  };
+}
